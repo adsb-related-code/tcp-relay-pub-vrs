aa0dc73239d9c75cc6da312644ee4037c4d31fbd
ADSBexchange.com <34561158+adsbxchange@users.noreply.github.com>
2018-08-25 16:24:42 -0700

    Update tcp-relay.go

--- tcp-relay.go
+++ tcp-relay.go
@@ -1,52 +1,4 @@
-package main
-
- import (
-         "fmt"
-         "net"
-         "bufio"
-         "log"
-         "os"
-  //       "bytes"
- )
-
-var TCPoutput = make(chan string)
-var buffer bytes.Buffer
-var clientCount = 0
-var allClients = make(map[net.Conn]int)
-
-func sendDataToClients(msg string){
-
-        //specific to ASDBx
-        //json object ]
-        //ReadString 
-        msg += "}"
- 
-        go func(){
-                for conn, _ := range allClients {
-
-                              //_, err := fmt.Fprintf(conn,"%s",msg)
-                                _, err := conn.Write([]byte(msg))
-
-
-                                if err != nil {
-                                                delete(allClients,conn)
-                                                clientCount -= 1
-                                }
-                                
-                }
-         }()
-
-      //clean up memory usage
-      //really should pull this out so I'm not writing to buffer and clearing it at the same time
-      //anyone help with this?
-      //buffer.Reset()
-}
-
-func handleConnection(conn net.Conn) {
-
-        clientCount += 1
-        allClients[conn] = clientCount
-        //fmt.Println("Handling new connection... ",clientCount) package main
+ package main
 
  import (
 	 "os"
@@ -98,8 +50,8 @@ func addtoConnMap(incoming net.Conn) {
 
 func main() {
 
-        hostName := "x.x.x.x"
-	portNum := "xxxxx"
+        hostName := "172.19.235.12"
+	portNum := "32001"
 
 	conn, err := net.Dial("tcp", hostName + ":" + portNum)
 	// exit on TCP connect failure
@@ -110,7 +62,7 @@ func main() {
         }
 	
 	// print error on listener error
-	server, err := net.Listen("tcp", ":xxxx")
+	server, err := net.Listen("tcp", ":6000")
     	if err != nil {
        	 	fmt.Println("Listener ERR: %s",err)
 		os.Exit(1)
@@ -151,75 +103,3 @@ func main() {
 	}
 
 }
-
-}
-
-
-func main() {
-
-        //public ASBX feed
-        hostName := "pub-vrs.adsbexchange.com"
-        portNum := "32005"
-
-        conn, err := net.Dial("tcp", hostName + ":" + portNum)
-        if err != nil {
-                fmt.Println(err)
-                os.Exit(1)
-        }
- 
-       /*
-       // going to modify for multiple listeners?!
-       // tcp server can push to this server instead of this server pulling
-       // for future design
-       
-       conn, err := net.Listen("tcp", ":32001")
-        if err != nil {
-            fmt.Println(err)
-            os.Exit(1)
-        }
-       */
-       go func() {
-
-                fmt.Printf("Connection established between %s and localhost.\n", hostName)
-                fmt.Printf("Remote Address : %s \n", conn.RemoteAddr().String())
-                fmt.Printf("Local Address : %s \n", conn.LocalAddr().String())
-        
-                // Add more statistics periodically printed out
-                //  go func() printing to terminal or outputin json on a port
-
-                for {
-                        status, err := bufio.NewReader(conn).ReadString(']')
-
-                        if err != nil {
-                                log.Fatal(err)
-                        }
-
-                      go sendDataToClients(status)
-
-                }
-
-        }()
-
-        server, err := net.Listen("tcp", ":6000")
-        if err != nil {
-            fmt.Println(err)
-            os.Exit(1)
-        }
-
-        for {
-                
-                conn, err := server.Accept()
-                if err != nil {
-                        fmt.Println(err)
-                        }
-
-                go handleConnection(conn)
-  
-          }
-
-}
-
-
-
-
-
