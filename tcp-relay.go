03d59a9094e785e9597a7392b8a8308f07a87384
ADSBexchange.com <34561158+adsbxchange@users.noreply.github.com>
2018-08-31 20:32:45 -0700

    revert back to Trim using "strings"
    msg += "}\r\n"
    msg = strings.TrimLeft(msg, "}")
    
    in place of ..
    
    if i == 1 { scan = scan[1:len(scan)] }
    
    Due to excessive memory usage - 8GB of ram and 4GB of swap used in 4 hours  :/

--- tcp-relay.go
+++ tcp-relay.go
@@ -76,7 +76,7 @@ func sendDataToClients(msg string) {
 	// VRS ADSBx specific since no newline is printed between data bursts
 	// we use ] and must add } closure
 	msg += "}\r\n"
-	//msg = strings.TrimLeft(msg, "}")
+	msg = strings.TrimLeft(msg, "}")
 
 	ctx, _ := context.WithTimeout(context.Background(), 2*time.Second)
 
@@ -127,7 +127,7 @@ func handleTCPIncoming(hostName string, portNum string) {
 		}
 	
 		// drop first { on every pass but first
-		if i == 1 { scan = scan[1:len(scan)] }
+		//if i == 1 { scan = scan[1:len(scan)] }
 
 		go sendDataToClients(scan)
 		i = 1
